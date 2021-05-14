aceb0928df44f2ee98969d342d7f602df0bfb03e
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-05-13 20:58:09 -0400

    Implementato l'orario stabilito per i messaggi. Implementato orario attuale nella creazione di nuovi messaggi.

--- vue.js
+++ vue.js
@@ -1,6 +1,7 @@
 new Vue ({
     el: "#root",
     data: {
+
         counter: 2,
         myMessage: "",
         answer: ["Non capisco...", "Guarda che non sono un intelligenza artificiale", "Parlassi col muro no capirebbe comunque piu di me", "Faccio solo finta di capire"],
@@ -13,16 +14,19 @@ new Vue ({
             messages: [
             {
             date: '10/01/2020 15:30:55',
+            time: "15:30",
             text: 'Hai portato a spasso il cane?',
             status: 'sent'
             },
             {
             date: '10/01/2020 15:50:00',
+            time: "15:50",
             text: 'Ricordati di dargli da mangiare',
             status: 'sent'
             },
             {
             date: '10/01/2020 16:15:22',
+            time: "16:15",
             text: 'Tutto fatto!',
             status: 'received'
             }
@@ -36,16 +40,18 @@ new Vue ({
             messages: [
             {
             date: '20/03/2020 16:30:00',
-            text: 'Ciao come stai?',
+            time: "16:30",
             status: 'sent'
             },
             {
             date: '20/03/2020 16:30:55',
+            time: "16:30",
             text: 'Bene grazie! Stasera ci vediamo?',
             status: 'received'
             },
             {
             date: '20/03/2020 16:35:00',
+            time: "16:35",
             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
             status: 'sent'
             }
@@ -58,16 +64,19 @@ new Vue ({
             messages: [
             {
             date: '28/03/2020 10:10:40',
+            time: "10:10",
             text: 'La Marianna va in campagna',
             status: 'received'
             },
             {
             date: '28/03/2020 10:20:10',
+            time: "10:20",
             text: 'Sicuro di non aver sbagliato chat?',
             status: 'sent'
             },
             {
             date: '28/03/2020 16:15:22',
+            time: "16:15",
             text: 'Ah scusa!',
             status: 'received'
             }
@@ -81,11 +90,13 @@ new Vue ({
             messages: [
             {
             date: '10/01/2020 15:30:55',
+            time: "15:30",
             text: 'Lo sai che ha aperto una nuova pizzeria?',
             status: 'sent'
             },
             {
             date: '10/01/2020 15:50:00',
+            time: "15:50",
             text: 'Si, ma preferirei andare al cinema',
             status: 'received'
             }
@@ -99,7 +110,8 @@ new Vue ({
     methods: {
         newMessage() {
             this.contacts[this.counter].messages.push({
-                date: '10/01/2020 15:50:00',
+                date: this.getDate(),
+                time: this.getHour(),
                 text: this.myMessage,
                 status: 'sent'
             })
@@ -113,7 +125,8 @@ new Vue ({
 
         newAnswer() {
             this.contacts[this.counter].messages.push({
-                date: '10/01/2020 15:50:00',
+                date: this.getDate(),
+                time: this.getHour(),
                 text: this.answer[Math.floor(Math.random() * (this.answer).length)],
                 status: 'received'
             })
@@ -126,12 +139,25 @@ new Vue ({
 
         changeCounter(param) {
             this.counter = param
+        },
+
+        getHour() {
+            let date = new Date();
+            let now = date.getHours() + ":" + date.getMinutes()
+            return now
+        },
+
+        getDate() {
+            let date = new Date
+            let today = date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear()
+            return today
         }
     },
 
 
 
     mounted() {
+        console.log(this.getDate())
     }
 })
 
